8cf604f116d0f72065a5f1ba816b7d79699fa3f5
Jacky Hao <jacky.hao18@gmail.com>
2021-09-12 14:44:00 -0500

    working on location forms

---
+++ app/forms/get_missions_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, DecimalField
+from wtforms.validators import DataRequired
+from app.models import db, User
+
+
+class GetMissionsForm(FlaskForm):
+    user_lat = DecimalField('user_lat', validators=[DataRequired()])
+    user_lng = DecimalField('user_lng', validators=[DataRequired()])
+    distance = IntegerField('distance', validators=[DataRequired()])
