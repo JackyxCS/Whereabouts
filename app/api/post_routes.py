15ce5e3826777dda801544f2134d41a6b1ef085e
Frema Awuku <frema.awuku@gmail.com>
2021-09-13 13:30:29 -0400

    attempting to get imports / flask app running

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -2,9 +2,15 @@ from flask import Blueprint
 from flask_login import login_required
 from app.models import Post, db
 
-posts = Blueprint('posts',__name__)
+post_routes = Blueprint('posts',__name__)
 
-@posts.route('/<int:id>')
+@post_routes.route('/<int:id>')
+@login_required
+def get_single_post(post_id):
+    post = Post.query.get(post_id)
+    return post.to_dict()
+
+@post_routes.route('/<int:id>')
 @login_required
 def get_single_post(post_id):
     post = Post.query.get(post_id)
