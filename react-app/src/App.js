7e11d69f1aca6587b0c9f5a1a99163a1884ccc62
Amanda Hinton <hello@amandahinton.com>
2021-09-14 17:44:36 -0700

    Style components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,7 @@ import LoginForm from './components/auth/LoginFormModal/LoginForm';
 import SignUpForm from './components/auth/SignUpFormModal/SignUpForm';
 import NavBar from './components/Nav/NavBar';
 import Footer from './components/Nav/Footer';
-import Homepage from './components/Homepage.js';
+import Homepage from './components/Homepage/Homepage.js';
 import ExploreAuth from './components/Explore/ExploreAuth.js';
 import ExplorePreview from './components/Explore/ExplorePreview.js';
 import ProtectedRoute from './components/auth/ProtectedRoute';
@@ -15,56 +15,56 @@ import UserLocationForm from './components/Missions/Missions';
 import { authenticate } from './store/session';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+    const [loaded, setLoaded] = useState(false);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+    useEffect(() => {
+        (async () => {
+        await dispatch(authenticate());
+        setLoaded(true);
+        })();
+    }, [dispatch]);
 
-  if (!loaded) {
-    return null;
-  }
+    if (!loaded) {
+        return null;
+    }
 
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        {/* <ProtectedRoute path='/users' exact={true} >
-          <UsersList />
-        </ProtectedRoute> */}
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/explore' exact={true} >
-          <ExploreAuth />
-        </ProtectedRoute>
-        <Route path='/preview' exact={true} >
-          <ExplorePreview />
-        </Route>
-        <Route path='/' exact={true} >
-          <Homepage />
-        </Route>
-
-        {/* switch to protect route later! */}
-        <ProtectedRoute path='/missions' exact={true}>
-          <h1>Missions!</h1>
-          <UserLocationForm />
-        </ProtectedRoute>
-
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+        <NavBar />
+        <div className="content-container">
+            <Switch>
+                <Route path='/login' exact={true}>
+                    <LoginForm />
+                </Route>
+                <Route path='/sign-up' exact={true}>
+                    <SignUpForm />
+                </Route>
+                {/* <ProtectedRoute path='/users' exact={true} >
+                <UsersList />
+                </ProtectedRoute> */}
+                <ProtectedRoute path='/users/:userId' exact={true} >
+                    <User />
+                </ProtectedRoute>
+                <ProtectedRoute path='/explore' exact={true} >
+                    <ExploreAuth />
+                </ProtectedRoute>
+                <Route path='/preview' exact={true} >
+                    <ExplorePreview />
+                </Route>
+                <Route path='/' exact={true} >
+                    <Homepage />
+                </Route>
+                {/* switch to protect route later! */}
+                <ProtectedRoute path='/missions' exact={true}>
+                    <h1>Missions!</h1>
+                    <UserLocationForm />
+                </ProtectedRoute>
+            </Switch>
+        </div>
+        <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
