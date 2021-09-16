dca0d052a2fa526c71c912ecec5a868612d3ddb8
Jacky Hao <jacky.hao18@gmail.com>
2021-09-16 01:33:30 -0500

    working on comments

--- react-app/src/components/Posts/PostDetail.js
+++ react-app/src/components/Posts/PostDetail.js
@@ -1,24 +1,25 @@
 import React, { useEffect } from "react";
 // import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getAllPosts } from '../../store/posts';
+import { fetchComments } from '../../store/comments';
 import "./posts.css"
 
 const PostDetail = () => {
 
     const dispatch = useDispatch()
 
-    const posts = useSelector(state => state.posts)
-
-    useEffect(() => {
-        dispatch(getAllPosts())
-    }, [dispatch])
-
     const { postId } = useParams();
+    const posts = useSelector(state => state.posts)
+    const comments = useSelector(state => state.comments)
+    const spotComments = comments.filter(comment => Number(comment.post_id) === Number(postId))
     const post = posts[postId];
 
-
+    useEffect(() => {
+        dispatch(fetchComments())
+        dispatch(getAllPosts())
+    }, [dispatch])
 
     if (post) {
         return (
