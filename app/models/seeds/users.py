8966e029908c6fbdf7b83768570945a830d7fb88
Amanda Hinton <hello@amandahinton.com>
2021-09-17 09:08:43 -0700

    Update usernames in db seeds

--- app/models/seeds/users.py
+++ app/models/seeds/users.py
@@ -2,7 +2,7 @@ from app.models import db, User
 
 def seed_users():
     demo = User(
-        username='Demo',
+        username='Guest',
         email='demo@aa.io',
         password='password',
         profile_picture="https://di2ponv0v5otw.cloudfront.net/posts/2020/09/20/5f6826773bad6d1d4c21b86f/m_5f682683e107bbb1727114e0.jpg",
@@ -11,7 +11,7 @@ def seed_users():
         user_radius=25
     )
     fremy = User(
-        username='fremy',
+        username='Fremy',
         email='fremy@aa.io',
         password='password',
         profile_picture="https://scontent-atl3-1.xx.fbcdn.net/v/t1.6435-9/155157747_10158480330872058_2091608857930148729_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UNIljRoawwYAX-6NeJb&_nc_ht=scontent-atl3-1.xx&oh=d7e66290459956118b7c2d42b8b3ad98&oe=6163C02A",
@@ -20,7 +20,7 @@ def seed_users():
         user_radius=15
     )
     wembley = User(
-        username='wembley',
+        username='Wembley',
         email='wembley@aa.io',
         password='password',
         profile_picture="https://images.unsplash.com/photo-1529582582744-13d8da049432?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
@@ -29,7 +29,7 @@ def seed_users():
         user_radius=5
         )
     amanda = User(
-        username='amanda',
+        username='Amanda',
         email='amanda@aa.io',
         password='password',
         profile_picture="https://images.squarespace-cdn.com/content/v1/5cca34c50cf57d8e7575f84b/1599705980190-4GXWADDT7BPLGBH1UCDM/AmandaHintonHeadshot.jpeg?format=1500w",
@@ -38,7 +38,7 @@ def seed_users():
         user_radius=10
     )
     bobbie = User(
-        username='bobbie',
+        username='Bobbie',
         email='bobbie@aa.io',
         password='password',
         user_lat=29.780350,
@@ -46,7 +46,7 @@ def seed_users():
         user_radius=50
     )
     jacky = User(
-        username='jacky',
+        username='Jacky',
         email='jacky@aa.io',
         password='password',
         user_lat=29.891970,
