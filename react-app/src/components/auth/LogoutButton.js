01f1ed468f7b31622be5fe5a3650212279c61641
Jacky Hao <jacky.hao18@gmail.com>
2021-10-14 16:00:30 -0500

    fix logout button

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
+  const history = useHistory()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/')
   };
 
   return <button className="secondary-button" onClick={onLogout}>Logout</button>;
